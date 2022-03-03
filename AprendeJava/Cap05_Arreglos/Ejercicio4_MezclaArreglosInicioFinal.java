9105dcb0e0bdd4065d2012a74bcc9e90c4459d5d
afigueroa1973 <afigueroa_s@outlook.com>
2022-03-02 22:02:59 -0600

    Agregando Sección 5 del Curso "Aprende Programación en Java" de Udemy

---
+++ AprendeJava/Cap05_Arreglos/Ejercicio4_MezclaArreglosInicioFinal.java
@@ -0,0 +1,36 @@
+package AprendeJava.Cap05_Arreglos;
+
+/*
+  Ejercicio 4:
+  Leer 10 números enteros, guardarlos en un arreglo. Debemos mostrarlos en el siguiente orden: el
+  primero, el último, el segundo, el penúltimo, etc.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio4_MezclaArreglosInicioFinal {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("\n** I M P R I M I R  I N I C I O / F I N A L **");
+    System.out.println("---------------------------------------------");
+
+    // Se crea un arreglo de 10 elementos
+    int[] numeros = new int[10];
+
+    // Pedimos el arreglo
+    for (int i = 0; i < 10; i++) {
+      System.out.print("Favor ingrese el elemento [" + (i + 1) + "]: ");
+      numeros[i] = input.nextInt();
+    }
+    // Imprimimos el arreglo
+    System.out.print("\nEl arreglo sería: ");
+    for (int i = 0; i < 5; i++) {
+      System.out.print(numeros[i] + ", "); // imprimirá el primer elemento
+      System.out.print(numeros[9 - i] + ", "); // Luego imprimirá el último elemento y asi sucesivamente
+    }
+    System.out.print("\b\b\n");
+
+    input.close(); // Se cierra la clase Scanner
+  }
+}
