e4059dff6cbd97ec0c3016ef643de767b49b5c8e
afigueroa1973 <afigueroa_s@outlook.com>
2022-03-14 10:10:27 -0600

    Agregando Sección 5 del Curso "Aprende Programación en Java" de Udemy

--- AprendeJava/Cap05_Arreglos/Ejercicio7_CrecienteDecreciente.java
+++ AprendeJava/Cap05_Arreglos/Ejercicio7_CrecienteDecreciente.java
@@ -15,11 +15,12 @@ public class Ejercicio7_CrecienteDecreciente {
     System.out.println("\n** C R E C I E N T E  O  D E C R E C I E N T E **");
     System.out.println("-------------------------------------------------");
 
-    //Creamos el arreglo de 10 elementos enteros
+    // Creamos el arreglo de 10 elementos enteros
     int[] arreglo = new int[10];
     // Creamos dos variables booleanas para confirmar si el arreglo es creciente o decreciente
     boolean creciente = false;
     boolean decreciente = false;
+    boolean iguales = false;
 
     // Llenamos el arreglo...
     System.out.println("Llenamos el arreglo...");
@@ -27,27 +28,30 @@ public class Ejercicio7_CrecienteDecreciente {
       System.out.print("Favor ingrese un número[" + (i + 1) + "]: ");
       arreglo[i] = input.nextInt();
     }
-    // Confirmamos si es creciente o decreciente
-    for (int i = 0; i < 9; i++) {
-      if (arreglo[i] < arreglo[i + 1]) { //Creciente: 1-2-3-4-5...
+    // Confirmamos si el arreglo va en orden creciente o decreciente
+    for (int i = 0; i < 9; i++) { // Se itera hasta 9, ya que si no "i + 1" saldría del arreglo
+      if (arreglo[i] < arreglo[i + 1]) { // Arreglo creciente: 1-2-3-4-5...
         creciente = true;
       }
-      if (arreglo[i] > arreglo[i + 1]) { //Decreciente: 5-4-3-2-1
+      if (arreglo[i] > arreglo[i + 1]) { // Arreglo decreciente: 5-4-3-2-1...
         decreciente = true;
       }
+      if (arreglo[i] == arreglo[i + 1]){ // Arreglos iguales: 1-1-1-1-1...
+        iguales = true;
+      }
     }
-
+    // Condicionales para imprimir resultados
     if (creciente && !decreciente) {
-      System.out.println("\nEl arreglo se encuentra en forma creciente");
+      System.out.println("\nEl arreglo se encuentra ordenado en forma creciente");
     }
     else if (!creciente && decreciente) {
-      System.out.println("\nEl arreglo se encuentra en forma decreciente");
+      System.out.println("\nEl arreglo se encuentra ordenado en forma decreciente");
     }
-    else if (creciente && decreciente) {
-      System.out.println("\nEl arreglo se encuentra desordenado");
+    else if (iguales) {
+      System.out.println("\nTodos los números del arreglo son iguales");
     }
     else
-      System.out.println("\nTodos los números del arreglo son iguales");
+    System.out.println("\nEl arreglo se encuentra desordenado");
 
     input.close(); // Se cierra la clase Scanner
   }
